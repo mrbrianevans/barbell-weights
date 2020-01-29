1eddf917bfb6bd55544c10e418a5b0e9d50cf2a3
Brian Evans <53117772+mrbrianevans@users.noreply.github.com>
2020-01-29 19:10:03 +0000

    Source code V1

---
+++ barbell_weights.py
@@ -0,0 +1,56 @@
+import itertools
+
+weights = [2, 5, 25]  # you can change the set of weights available
+
+
+def unrestricted_combinations(total_weight):
+    """Calculates the different combination of weights on a barbel to reach a target weight"""
+    max_of_each_weight = [[*range(0, total_weight // weight // 2 * 2 + 1, 2)] for weight in weights]
+    return [comb for comb in itertools.product(*max_of_each_weight) if attempt(comb, total_weight)]
+
+
+def attempt(combination: tuple, target_weight: int):
+    """Tests if a certain combination of weights achieves the desired total and returns a boolean"""
+    total_weight = 0
+    for i in range(len(weights)):
+        total_weight += weights[i] * combination[i]
+    return target_weight == total_weight
+
+
+# From here on is just testing the above method and printing solutions
+
+
+def check_answers(solutions, target_weight):
+    for solution in solutions:
+        temp_weight = 0
+        for i in range(len(weights)):
+            temp_weight += solution[i] * weights[i]
+        if not target_weight == temp_weight:
+            return False
+    return True
+
+
+def test_answers(solutions, target_weight):
+    if check_answers(solutions, target_weight):
+        print("Test PASSED!")
+    else:
+        print("Test FAILED")
+
+
+def print_solutions(solutions):
+    print(len(solutions), 'possible combinations found')
+    for solution in solutions:
+        temp_weight = 0
+        for i in range(len(weights)):
+            temp_weight += solution[i] * weights[i]
+            print(solution[i], 'x', str(weights[i]) + 'kg', end="   ")
+        print('=', str(temp_weight) + 'kg')
+
+
+if __name__ == "__main__":
+
+    target = 130  # try adjusting this number
+
+    test_solutions = unrestricted_combinations(target)
+    print_solutions(test_solutions)
+    test_answers(test_solutions, target)
